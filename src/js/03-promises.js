46e563acaa29a971a3fab3e41739fb202f610ec8
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-04-05 23:18:28 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+const formRef = document.querySelector('.form');
+const [delayRef, stepRef, amountRef] = document.querySelectorAll(
+  'input[name="delay"], input[name="step"], input[name="amount"]'
+);
+
+formRef.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let delay = Number(delayRef.value);
+  const step = Number(stepRef.value);
+  const amount = Number(amountRef.value);
+  
+
+  for (let index = 1; index <= amount; index += 1) {
+    createPromise(index, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
 }
